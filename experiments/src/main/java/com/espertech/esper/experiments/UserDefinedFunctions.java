e16b778cf1abd5a072201f41342e378d150b8c67
Espen Volnes <espen.volnes@gmail.com>
2020-06-11 00:34:01 +0200

    double to long

--- experiments/src/main/java/com/espertech/esper/experiments/UserDefinedFunctions.java
+++ experiments/src/main/java/com/espertech/esper/experiments/UserDefinedFunctions.java
@@ -1,7 +1,7 @@
 package com.espertech.esper.experiments;
 
 public class UserDefinedFunctions {
-    public static double computeDolToEur(double price) {
-        return price * 0.89;
+    public static long computeDolToEur(long price) {
+        return (long) (price * 0.89);
     }
 }
